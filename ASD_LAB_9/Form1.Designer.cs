3f7489a2606c9648ef46e2e444a28e408778a18a
illya06 <47461573+illya06@users.noreply.github.com>
2020-12-14 18:51:38 +0200

    work_done

--- ASD_LAB_9/Form1.Designer.cs
+++ ASD_LAB_9/Form1.Designer.cs
@@ -28,34 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.find = new System.Windows.Forms.Button();
+            this.size = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.value = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // find
             // 
-            this.button1.Location = new System.Drawing.Point(379, 69);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.find.Location = new System.Drawing.Point(114, 167);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(48, 23);
+            this.find.TabIndex = 0;
+            this.find.Text = "FIND";
+            this.find.UseVisualStyleBackColor = true;
+            this.find.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // size
+            // 
+            this.size.Location = new System.Drawing.Point(172, 92);
+            this.size.Name = "size";
+            this.size.Size = new System.Drawing.Size(38, 20);
+            this.size.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SIZE : ";
+            // 
+            // value
+            // 
+            this.value.Location = new System.Drawing.Point(172, 169);
+            this.value.Name = "value";
+            this.value.Size = new System.Drawing.Size(38, 20);
+            this.value.TabIndex = 3;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(362, 130);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(73, 20);
+            this.result.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RESULT : ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.value);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.size);
+            this.Controls.Add(this.find);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button find;
+        private System.Windows.Forms.TextBox size;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox value;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ASD_LAB_9/Form1.cs
+++ ASD_LAB_9/Form1.cs
@@ -21,12 +21,24 @@ namespace ASD_LAB_9
         {
             Random rnd = new Random();
             List<int> arr = new List<int>();
-            for(int i = 0; i < 10; i++)
+
+            int n = Convert.ToInt32(size.Text);
+            for (int i = 0; i < n; i++)
             {
                 arr.Add(rnd.Next(-10, 10));
             }
 
             arr.Sort();
+            foreach(int i in arr)
+            {
+                if (i > 0)
+                {
+                    arr[arr.IndexOf(i)] = 0;
+                    break;
+                }
+            }
+
+            result.Text = $"{Binary.binSearch(arr.ToArray(), Convert.ToInt32(value.Text), 0, arr.Count - 1)}"; 
         }
     }
 }
